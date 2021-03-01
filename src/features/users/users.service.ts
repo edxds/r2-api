7faa962240aa89316ba7a99f80c0d6fd150347bb
Eduardo Souza <edux404@gmail.com>
2021-03-01 01:04:06 -0300

    feat(users): add social login support

--- src/features/users/users.service.ts
+++ src/features/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-import { User } from './users.types';
+import { MinimalSocialProfile, User } from './users.types';
 
 @Injectable()
 export class UsersService {
@@ -11,5 +11,28 @@ export class UsersService {
 
   async findByUsername(username: string): Promise<User | undefined> {
     return this.users.find((u) => u.username === username);
+  }
+
+  async findBySocialIdOrCreate(
+    socialId: string,
+    profile?: MinimalSocialProfile,
+  ): Promise<User | undefined> {
+    const user = this.users.find((u) => u.socialId === socialId);
+    if (!user) {
+      if (!profile) {
+        return;
+      }
+
+      const newUser = {
+        username: '__undefined',
+        ...profile,
+        socialId: profile.id,
+        id: this.users[this.users.length - 1].id + 1,
+      };
+
+      this.users.push(newUser);
+      console.log(this.users);
+      return newUser;
+    }
   }
 }
--- src/features/users/users.types.ts
+++ src/features/users/users.types.ts
@@ -2,5 +2,13 @@ export type User = {
   id: number;
   username: string;
   email: string;
-  password: string;
+  password?: string;
+  socialId?: string;
+};
+
+export type MinimalSocialProfile = {
+  id: string;
+  name?: string;
+  username?: string;
+  email: string;
 };
