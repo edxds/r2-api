8f6ee727d8bc6db5b22493ffac28403b553e20ac
Eduardo Souza <edux404@gmail.com>
2021-02-28 23:48:53 -0300

    chore: remove @nestjs/config and use dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import config from '../ormconfig';
@@ -9,7 +8,7 @@ import { AppService } from './app.service';
 import { TimeService } from './time.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService, TimeService],
 })
