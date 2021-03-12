6623d3d4ab71706a3b492b7ad4e861ec72157b17
Eduardo Souza <edux404@gmail.com>
2021-03-11 23:40:18 -0300

    feat: set up cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3000', credentials: true },
+  });
+
   app.use(cookieParser());
   await app.listen(8080);
 }
