cc585c47ddf00d8fd605efc725fecabc76bb5f0f
Eduardo Souza <edux404@gmail.com>
2021-03-14 12:40:49 -0300

    feat: add cors rule for local ip address

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import * as ip from 'ip';
 
 import { AppModule } from './app.module';
 
+const PORT = 8080;
+const FRONTEND_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'http://localhost:3000', credentials: true },
+    cors: {
+      origin: ['http', 'https'].flatMap((protocol) =>
+        ['localhost', ip.address()].map((address) => `${protocol}://${address}:${FRONTEND_PORT}`),
+      ),
+      credentials: true,
+    },
   });
 
   app.use(cookieParser());
-  await app.listen(8080);
+  await app.listen(PORT);
 }
 
 bootstrap();
